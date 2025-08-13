c8179c36e3014b1bd2f4d7ef5f4235f56f9675ee
Chris Henrick <chrishenrick@gmail.com>
2025-08-13 08:58:01 -0700

    fix: updated jsdoc for getHomePageLinks fn

--- website/src/_utils/homePageLinks.js
+++ website/src/_utils/homePageLinks.js
@@ -1,7 +1,13 @@
 /**
+ * @typedef {Object} HomeLinksDatum
+ * @property {string} resources - absolute path to resources page
+ * @property {string} why - absolute path to why page
+ */
+
+/**
  * gets the in page links to the resources and why pages
- * @param {{}} data
- * @returns {{} | undefined}
+ * @param {{}} data - data that is passed to eleventyComputed
+ * @returns {HomeLinksDatum | undefined}
  */
 export const getHomePageLinks = (data) => {
   const lang = data.lang;
